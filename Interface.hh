28f96868b6d02b7bf832e4e46f551610d3aba250
Hamza Awad <hawad003@ucr.edu>
2017-10-29 13:11:46 -0700

    Created the abstract class as header file with no implementation

---
+++ Interface.hh
@@ -0,0 +1,13 @@
+#ifndef Interface_hh
+#define Interface_hh
+
+class Interface {
+   private:
+      
+   public:
+      Interface() {};
+      ~Interface() {};
+      virtual void execute() = 0;
+};
+
+#endif
