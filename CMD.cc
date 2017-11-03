5a99bacef4adcd0cefc93015c3b03d462e33d451
Hamza Awad <hawad003@ucr.edu>
2017-11-03 04:24:14 -0700

    I did something

---
+++ CMD.cc
@@ -0,0 +1,38 @@
+#include "CMD.hh"
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+CMD::CMD() : instruction(""), connector(0) {}
+
+CMD::~CMD() {}
+
+CMD::CMD(std::string instruction, int connector) : instruction(instruction), connector(connector) {}
+
+int CMD::getConnector(){
+	return connector;
+}
+
+string CMD::getInstruction(){
+	/*
+	istringstream inSS(instruction);
+	string curr = "";
+	vector<char*> v;
+	while(inSS >> curr){
+	   v.push_back((char*)curr.c_str());
+        }
+	
+
+//	char* args[25];
+	char* args;
+	for(int i = 0; i < v.size(); i++){
+		args[i] = *v.at(i);
+	}
+	args[v.size()] = 0;
+	return args;
+	*/
+	
+	//return instruction.c_str();
+	return instruction;
+}
