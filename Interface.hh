345fab9b4e27fadc9a80d1a92940c27681fbd8ef
hsimj001 <hsimj001@ucr.edu>
2017-11-02 19:33:01 -0700

    Changed interface to work with the newly changed Command class

--- Interface.hh
+++ Interface.hh
@@ -6,10 +6,13 @@
 
 class Interface {
    private:
-      
+      vector<int> connectors;
+      vector<vector<char*>> commands;     
    public:
       Interface();
       ~Interface();
+      void changeConVec(vector<int>);
+      void changeCommandVec(vector<vector<char*>>);
       void orchestrate();
 };
 
