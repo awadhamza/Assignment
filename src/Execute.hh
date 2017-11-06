41fd16579e40a858eae67da5020c4f03d7ecc2f6
Hamza Awad <hawad003@ucr.edu>
2017-11-05 16:46:23 -0800

    Removed source files from repository

--- src/Execute.hh
+++ src/Execute.hh
@@ -1,16 +1,20 @@
 #ifndef EXECUTE_HH
 #define EXECUTE_HH
 
+#include <string>
+
 #include <vector>
 #include "CMD.hh"
 
 class Execute {
 	private:
-	
+		char** executables;
+		int executablesSize = 0;
 	public:
 		Execute();
 		~Execute();
 		void execute(std::vector<CMD*> );
+		char* makeCharPointers(std::vector<CMD*> );
 		
 };
 
