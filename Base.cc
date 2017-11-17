822728c697818b4b3ef7ccbe9f2a91fe822a034c
Hamza Awad <hawad003@ucr.edu>
2017-11-17 11:30:32 -0800

    Forgot to commit this

---
+++ Base.cc
@@ -0,0 +1,23 @@
+#include "src/Base.hh"
+
+void Base::changeConnector(int newConnector){
+		connector = newConnector;
+		return;
+}
+
+int Base::getConnector(){
+	return connector;
+}
+
+int Base::getDone(){
+	return done;
+}
+
+void Base::setDone(int newDone){
+	done = newDone;
+	return;
+}
+
+std::string Base::getInstruction(){
+	return instruction;
+}
