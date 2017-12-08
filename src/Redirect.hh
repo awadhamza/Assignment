a114d78f7bd15e2fd2d3d54f0b8ae135f441330e
Hamza Awad <hawad003@ucr.edu>
2017-12-08 10:39:33 -0800

    Created basic Redirect structure

---
+++ src/Redirect.hh
@@ -0,0 +1,21 @@
+#ifndef REDIRECT_HH
+#define REDIRECT_HH
+
+#include <vector>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+
+class Redirect : public Base {
+	public:
+		Redirect() : Base() {}
+		~Redirect();
+		void execute(;
+		bool is_Group();
+		
+};
+
+#endif
