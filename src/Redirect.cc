f8d6046b8e0e25a9826359eb0965ae603c9b108d
hsimj001 <hsimj001@ucr.edu>
2017-12-08 23:01:54 -0800

    made the first execute, executepipe

--- src/Redirect.cc
+++ src/Redirect.cc
@@ -54,9 +54,85 @@ void Redirect::execute(string fullCommand, int connector)
 	
 }
 
-void executePipe(string , string )
+void executePipe(string command, string secondCommand)
 {
+	string completeCommand = command;
+  string completeCommand1 = secondCommand;
+  char splitCommand[1024];
+  char *executables[1024];
+  char splitCommand1[1024];
+  char *executables1[1024];
+  int charSize = completeCommand.size();
+  int charSize1 = completeCommand1.size();
+  char* addLetter = NULL;
+  int i = 0;
+  int executablesIndex = 0;
+  char* addLetter1 = NULL;
+  int i1 = 0;
+  int executablesIndex1 = 0;
+  
+  for (unsigned int j = 0; j < completeCommand.size(); j++){
+    splitCommand[j] = completeCommand[j];
+  }
+  
+  splitCommand[charSize] = '\0';
+  
+  addLetter = strtok(splitCommand, " " );
+  
+  while(addLetter != NULL){
+    executables[i++] = addLetter;
+    addLetter = strtok(NULL, " ");
+    executablesIndex++;
+  }
+  
+  for (unsigned int j1 = 0; j1 < completeCommand1.size(); j1++){
+    splitCommand1[j1] = completeCommand1[j1];
+  }
+  
+  splitCommand1[charSize1] = '\0';
+  
+     addLetter1 = strtok(splitCommand1, " ");    
+         
+           while(addLetter1 != NULL){
+               executables1[i1++] = addLetter1;
+               addLetter1 = strtok(NULL, " ");
+               executablesIndex1++;
+           }
+
+    int     nbytes, ret, size, length, Descriptor1, Descriptor2, saveSTDIN, saveSTDOUT, writenum    ;
+    int fd[2];
+
+    pid_t   childpid;
+
+string line, cmd1, cmd2;
+char* input[MAX_ARGS];
+int FD[2];
+
+pipe(FD);
+        
+pid_t pid = fork();
+if( pid == 0 ){
+        close(FD[0]); //close pipe output
+        dup2(FD[1], 1); //copy pipe input to stdout
 	
+	if(contains(completeCommand, '|') || contains(completeCommand, '<') || contains(completeCommand, '>')){
+		Redirect* temp = new Redirect(completeCommand, 0);
+		temp.execute();
+	}
+	else{
+		if(execvp(splitCommand, executables) == -1){
+             	 	cout << "FAILED" << endl;
+            	}
+        }
+        }
+ else 
+        {
+            close(FD[1]); //close pipe input
+            dup2(FD[0], 0); //copy pipe output to stdin
+	  if(execvp(splitCommand1, executables1)){
+              cout << "FAILED" << endl;
+            }
+        }
 }
 void executeGG(string , string )
 {
